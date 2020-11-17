6842f6734bbbfc053b0b636f16d3d746ac4cc10b
GACOSTA1988 <georgeacostar@gmail.com>
2020-11-16 22:23:23 -0800

    add postsSlice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: '1', title: 'first post', content: 'Hello fooo!'}, 
+  {id: '2', title: 'second post', content: 'Supppp fooo'}
+]
+
+const postSlice = createSlice({
+  name: 'posts', 
+  initialState, 
+  reducers: {}
+})
